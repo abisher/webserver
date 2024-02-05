a06803751491700141784254120027435775b959
zoya <zoyamax174@gmail.com>
2024-02-05 11:59:37 +0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::fmt::format;
+use actix_web::{web, App, HttpServer, Responder,
+                HttpRequest};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name")
+        .unwrap_or("World!");
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        println!("http server factory is firing!");
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/say/hello", web::get().to(||
+                async { "Hello Again!" }))
+    })
+        .bind("127.0.0.1:8080")?
+        .workers(3)
+        .run()
+        .await
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+    use std::thread::JoinHandle;
+    use std::time::{Duration, Instant};
+
+
+    #[test]
+    fn test() {
+        fn do_something(number: i8) -> i8 {
+            println!("number {number} is running");
+            let two_secs = Duration::new(2, 0);
+            thread::sleep(two_secs);
+            2
+        }
+
+        let now = Instant::now();
+        let thread_one: JoinHandle<i8> = thread::spawn(|| do_something(1));
+        let thread_two: JoinHandle<i8> = thread::spawn(|| do_something(2));
+        let thread_three: JoinHandle<i8> = thread::spawn(|| do_something(3));
+
+        let res_one = thread_one.join().expect("Something went wrong!");
+        let res_two = thread_two.join().expect("Something went wrong!");
+        let res_three = thread_three.join().expect("Something went wrong!");
+
+        println!("time elapsed : {:?}", now.elapsed());
+        println!("result: {}", res_one + res_two + res_three);
+    }
+}
