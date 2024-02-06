558f27e6356dae1456840ef914c5987ad7e561ca
INSP1RIT <inspiritstillok@protonmail.com>
2024-02-06 12:18:55 +0300

    basic app

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
-use std::fmt::format;
-use actix_web::{web, App, HttpServer, Responder,
-                HttpRequest};
+mod views;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name")
-        .unwrap_or("World!");
-    format!("Hello {}!", name)
-}
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        println!("http server factory is firing!");
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/say/hello", web::get().to(||
-                async { "Hello Again!" }))
+
+        let app = App::new().configure(views::views_factory);
+        app
     })
         .bind("127.0.0.1:8080")?
-        .workers(3)
         .run()
         .await
 }
