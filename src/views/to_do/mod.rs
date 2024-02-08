f5df2b854020d66a85828c491b279c7710b3eb6f
R1zam <maxalex324@gmail.com>
2024-02-08 23:48:15 +0300

    some new

--- src/views/to_do/mod.rs
+++ src/views/to_do/mod.rs
@@ -1,8 +1,11 @@
 mod create;
+mod get;
 
 use actix_web::web::{ServiceConfig, get, scope, post};
 
 pub fn to_do_views_factory(app: &mut ServiceConfig) {
     app.service(scope("v1/item")
-        .route("create/{title}", post().to(create::create)));
+        .route("create/{title}", post().to(create::create))
+        .route("get", get().to(get::get))
+        );
 }
