14f382402d57a0c3559212942bd5d0c98ce2eb93
R1zam <maxalex324@gmail.com>
2024-02-18 00:15:56 +0300

    some new

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        "message" : "To Do"
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <p>{this.state.message} application</p>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App; 
