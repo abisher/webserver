9b3167024ce7152300865b44af6cd75ea053b405
zoya <zoyamax174@gmail.com>
2024-02-18 11:03:35 +0300

    states

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,57 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class App extends Component {
     state = {
-        "message" : "To Do"
+        "pending_items": [],
+        "done_items": [],
+        "pending_items_count": 0,
+        "done_items_count": 0
+    }
+
+    // makes the API call
+    getItems() {
+        axios.get("https://127.0.0.1:8000/v1/item/get",
+            {headers: {"token": "some_token"}})
+            .then(response => {
+                let pendingItems = response.data["pending_items"]
+                let done_items = response.data["done_items"]
+
+                this.setState({
+                    "pending_items": this.processItemValues(pendingItems),
+                    "done_items": this.processItemValues(done_items),
+                    "pending_items_count": response.data["pending_items_count"],
+                    "done_items_count": response.data["done_items_count"]
+                })
+            })
+    }
+
+    // ensures the API call is updated than mounted
+    componentDidMount() {
+        this.getItems();
+    }
+
+    // converts items from API to HTML
+    processItemValues(items) {
+        let itemList = [];
+        items.forEach((item, index) => {
+            itemList.push(
+                <li key={index}>{item.title} {item.status}</li>
+            )
+        })
+
+        return itemList
     }
 
     render() {
         return (
             <div className="App">
-                <p>{this.state.message} application</p>
+                <h1>Done Items</h1>
+                <p>done item count: {this.state.done_items_count}</p>
+                {this.state.done_items}
+                <h1>Pending Items</h1>
+                <p>pending item count: {this.state.pending_items_count}</p>
+                {this.state.pending_items}
             </div>
         )
     }
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ mod jwt;
 
 use actix_service::Service;
 use actix_web::{App, HttpServer};
+use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allow_any_method()
+            .allow_any_origin();
 
         let app = App::new()
             .wrap_fn(|req,srv| {
@@ -22,7 +27,7 @@ async fn main() -> std::io::Result<()> {
                     Ok(result)
                 }
             })
-            .configure(views::views_factory);
+            .configure(views::views_factory).wrap(cors);
         app
     })
         .bind("127.0.0.1:8080")?
